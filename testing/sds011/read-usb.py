15c492694ed2bb8468ca674b9c41ab673396d6db
Jack <git@jackhiggins.ie>
2020-04-30 18:23:15 +0200

    testing code for AQ sensor

---
+++ testing/sds011/read-usb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# runs on Python 2. comes from https://gist.github.com/geoffwatts/b0b488b5a5257223ed53
+
+import serial, time, struct
+
+ser = serial.Serial()
+ser.port = "/dev/cu.wchusbserial1420" # Set this to your serial port
+ser.baudrate = 9600
+
+ser.open()
+ser.flushInput()
+
+byte, lastbyte = "\x00", "\x00"
+
+while True:
+    lastbyte = byte
+    byte = ser.read(size=1)
+    
+    # We got a valid packet header
+    if lastbyte == "\xAA" and byte == "\xC0":
+        sentence = ser.read(size=8) # Read 8 more bytes
+        readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
+        
+        pm_25 = readings[0]/10.0
+        pm_10 = readings[1]/10.0
+        # ignoring the checksum and message tail
+        
+        print "PM 2.5:", pm_25, "μg/m^3  PM 10:", pm_10, "μg/m^3"
